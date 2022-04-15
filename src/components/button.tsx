80fc7c42d1a72a155b8af33bda3f4d797bca519b
Bahar <bahar@firstsource.tech>
2022-04-15 12:30:31 +0800

    add_test

---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+
+const Button = () => {
+  return (
+    <div>
+        <button type="submit" onClick={()=> console.log("test")} >Generate Email</button>
+    </div>
+  )
+}
+
+export default Button;
